84cb32078ec61c126e76809be29ec411a45e2d43
Elijah wines <elijahwines84@yahoo.com>
2020-07-13 19:22:34 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,15 @@
 class SessionsController < ApplicationController
 
-  def new
-    
+  def create
+    if params[:name].blank?
+      redirect_to '/login'
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name
   end
 end
